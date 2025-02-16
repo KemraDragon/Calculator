a47b4593402986a8d0cf18ab6d78d881c11593b2
KemraDragon <94020099+KemraDragon@users.noreply.github.com>
2025-02-16 13:47:48 -0800

    Calculator
    Calculator created from a Youtube Tutorial from CodeBrah

---
+++ Calculator/ CalcControllerViewModel.swift
@@ -0,0 +1,23 @@
+//
+//   CalcControllerViewModel.swift
+//  Calculator
+//
+//  Created by Kevin on 16-02-25.
+//
+
+import Foundation
+
+class CalcControllerViewModel {
+
+    let calcButtonCells: [CalculatorButton] = [
+        .allClear, .plusMinus, .percentage, .divide,
+        .number(7), .number(8), .number(9), .multiply,
+        .number(4), .number(5), .number(6), .subtract,
+        .number(1), .number(2), .number(3), .add,
+        .number(0), .decimal, .equals
+    ]
+
+    private(set) lazy var calcHeaderLabel: String = "42"
+
+}
+
